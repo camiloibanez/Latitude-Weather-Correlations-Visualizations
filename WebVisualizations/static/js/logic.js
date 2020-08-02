0dc8289c81ad2ec4ac08241af3e18cc015698c5c
camiloibanez <camiloibanez8@gmail.com>
2020-08-02 17:01:43 -0500

    markers

--- WebVisualizations/static/js/logic.js
+++ WebVisualizations/static/js/logic.js
@@ -1,13 +1,35 @@
-var myMap = L.map("map", {
-    center: [0, 0],
-    zoom: 2
-});
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
-}).addTo(myMap);
+});
+
+var array;
+
+$.get("../../Resources/assets/cities.csv", function(data, status) {
+    array =$.csv.toObjects(data);
+    console.log(array);
+});
+
+var cityMarkers = [];
+
+for (var i=0; i < array.length; i++) {
+    cityMarkers.push(L.marker([array[i]["Lat"], array[i]["Lng"]]).bindPopup("<h2>" + array[i]["City"] + "</h2>"));
+};
+
+var cityLayer = L.layerGroup(cityMarkers);
+
+var baseMaps = {"Street View" : streets};
+
+var overlayMaps = {Cities: cityLayer};
+
+var myMap = L.map("map", {
+    center: [15, 0],
+    zoom: 2,
+    layers = [streets, cityLayer]
+});
+
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
