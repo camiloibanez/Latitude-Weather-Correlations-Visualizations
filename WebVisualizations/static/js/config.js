b82367de75be1e80fa2472d096154b16418059a3
camiloibanez <camiloibanez8@gmail.com>
2020-08-02 12:13:19 -0500

    map tile layer

---
+++ WebVisualizations/static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoiY2FtaWxvaSIsImEiOiJja2J5MmFuMWowZGZzMnNwanl3aHhrMXlkIn0.T5o02VrPvl-2Qq7hzbIqHg"
---
+++ WebVisualizations/static/js/logic.js
@@ -0,0 +1,13 @@
+var myMap = L.map("map", {
+    center: [0, 0],
+    zoom: 2
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(myMap);
