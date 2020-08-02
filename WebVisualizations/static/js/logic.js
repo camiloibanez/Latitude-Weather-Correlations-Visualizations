68567a422b26f86930eb4a07b68cc6c0566f992d
camiloibanez <camiloibanez8@gmail.com>
2020-08-02 17:34:55 -0500

    added popup info and more tile layers

--- WebVisualizations/static/js/logic.js
+++ WebVisualizations/static/js/logic.js
@@ -7,6 +7,24 @@ var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
 cities_url = "https://camiloibanez.github.io/Latitude-Weather-Correlations-Visualizations/WebVisualizations/Resources/cities.csv"
 
 var array;
@@ -19,12 +37,21 @@ var cityMarkers = [];
 
 setTimeout(function() {
     for (var i=0; i < array.length; i++) {
-        cityMarkers.push(L.marker([array[i]["Lat"], array[i]["Lng"]]).bindPopup("<h2>" + array[i]["City"] + "</h2>"));
+        cityMarkers.push(L.marker([array[i]["Lat"], array[i]["Lng"]]).bindPopup(
+            "<h2 class='header'>" + array[i]["City"] + "</h2>" +
+            "<hr>" + 
+            "<h6 class='header'>Max Temp: " + array[i]["Max Temp"] + "</h6>" +
+            "<h6 class='header'>Humidity: " + array[i]["Humidity"] + "</h6>" +
+            "<h6 class='header'>Wind Speed: " + array[i]["Wind Speed"] + "</h6>")
+            );
     };
 
     var cityLayer = L.layerGroup(cityMarkers);
 
-    var baseMaps = {"Street View" : streets};
+    var baseMaps = {"Street View" : streets,
+        "light View" : light,
+        "Dark View" : dark
+    };
 
     var overlayMaps = {Cities: cityLayer};
 
@@ -35,4 +62,4 @@ setTimeout(function() {
     });
 
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-}, 2000);
+}, 1000);
